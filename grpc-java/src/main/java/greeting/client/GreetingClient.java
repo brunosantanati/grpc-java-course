4682aa7693f35487ba4c4593f212eba9bd63bcd8
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-04-19 10:54:04 -0300

    Implement what was missing in the greet client.

--- grpc-java/src/main/java/greeting/client/GreetingClient.java
+++ grpc-java/src/main/java/greeting/client/GreetingClient.java
@@ -1,20 +1,40 @@
 package greeting.client;
 
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class GreetingClient {
 
     public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("Need one argument to work");
+            return;
+        }
+
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
-        //do something
+        switch (args[0]){
+            case "greet": doGreet(channel); break;
+            default:
+                System.out.println("Invalid Keyword: " + args[0]);
+        }
 
         System.out.println("Shutting down");
         channel.shutdown();
+    }
+
+    private static void doGreet(ManagedChannel channel) {
+        System.out.println("Enter doGreet");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("Bruno").build());
+
+        System.out.println("Greeting: " + response.getResult());
     }
 
 }

20dea30e61f229bf30bf7fdd5f69e2d94814569f
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-04-26 14:58:41 -0300

    gRPC Client Streaming Example.

--- grpc-java/src/main/java/greeting/client/GreetingClient.java
+++ grpc-java/src/main/java/greeting/client/GreetingClient.java
@@ -5,10 +5,17 @@ import com.proto.greeting.GreetingResponse;
 import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class GreetingClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if(args.length == 0){
             System.out.println("Need one argument to work");
             return;
@@ -22,6 +29,7 @@ public class GreetingClient {
         switch (args[0]){
             case "greet": doGreet(channel); break;
             case "greet_many_times": doGreetManyTimes(channel); break;
+            case "long_greet": doLongGreet(channel); break;
             default:
                 System.out.println("Invalid Keyword: " + args[0]);
         }
@@ -45,6 +53,46 @@ public class GreetingClient {
         stub.greetManyTimes(GreetingRequest.newBuilder().setFirstName("Bruno").build()).forEachRemaining(response -> {
             System.out.println(response.getResult());
         });
+    }
+
+    private static void doLongGreet(ManagedChannel channel) throws InterruptedException {
+        System.out.println("Enter doLongGreet");
+        GreetingServiceGrpc.GreetingServiceStub stub = GreetingServiceGrpc.newStub(channel);
+
+        List<String> names = new ArrayList<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Collections.addAll(names, "Bruno", "Piper", "Carson");
+
+        StreamObserver<GreetingRequest> stream = stub.longGreet(new StreamObserver<GreetingResponse>() {
+            @Override
+            public void onNext(GreetingResponse value) {
+                // we get a response from the server
+                // onNext will be called only once
+                System.out.println("Received a response from the server");
+                System.out.println(value.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // we get an error from the server
+            }
+
+            @Override
+            public void onCompleted() {
+                // the server is done sending us data
+                // onCompleted will be called right after onNext()
+                System.out.println("Server has completed sending us something");
+                latch.countDown();
+            }
+        });
+
+        for(String name : names){
+            stream.onNext(GreetingRequest.newBuilder().setFirstName(name).build());
+        }
+
+        stream.onCompleted();
+        latch.await(3, TimeUnit.SECONDS);
     }
 
 }

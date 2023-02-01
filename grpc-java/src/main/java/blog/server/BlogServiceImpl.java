ef8d5d9ed03294be4b7ca7d9f5431706fc07c8d9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-02-01 15:29:05 -0300

    Implement updateBlog feature on server side

--- grpc-java/src/main/java/blog/server/BlogServiceImpl.java
+++ grpc-java/src/main/java/blog/server/BlogServiceImpl.java
@@ -1,5 +1,6 @@
 package blog.server;
 
+import com.google.protobuf.Empty;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -14,6 +15,8 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.set;
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
@@ -80,6 +83,37 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
                 .setTitle(result.getString("title"))
                 .setContent(result.getString("content"))
                 .build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateBlog(Blog request, StreamObserver<Empty> responseObserver) {
+        if (request.getId().isEmpty()) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription("The blog ID cannot be empty")
+                    .asRuntimeException());
+            return;
+        }
+
+        String id = request.getId();
+        Document result = mongoCollection.findOneAndUpdate(
+                eq("_id", new ObjectId(id)),
+                combine(
+                        set("author", request.getAuthor()),
+                        set("title", request.getTitle()),
+                        set("content", request.getContent())
+                )
+        );
+
+        if (result == null) {
+            responseObserver.onError(Status.NOT_FOUND
+                    .withDescription("Blog was not found")
+                    .augmentDescription("BlogId: " + id)
+                    .asRuntimeException());
+            return;
+        }
+
+        responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
     }
 }
